ca19cc7b90e81fc02b2cee58fea7bead9dfccb1c
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-04-13 17:38:46 +0100

    started drafting script

---
+++ inst/scripts/semver_script.R
@@ -0,0 +1,14 @@
+# This script is used to test the functionality of packages that work with
+# semantic versioning.
+
+# The two packages used in this script are {semver} and {semverutils}. Both of
+# these packages are dependencies of {lifecycling} and are available from CRAN
+
+# This script is not a vignette as the {lifecycling} package is not aimed to
+# showcase the functionality of these packages, and this script is aimed at
+# understanding what these packages offer
+
+library(semver)
+library(semverutils)
+
+
